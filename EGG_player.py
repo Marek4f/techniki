00323d97486ef822352e23111727dd8353d5cb4c
Marek E <marekegg94@gmail.com>
2014-05-09 21:15:48 +0200

    nie dziala

--- EGG_player.py
+++ EGG_player.py
@@ -5,44 +5,37 @@ from PyQt4.QtGui import *
 from PyQt4.phonon import *
 import time
 
+app = QApplication(sys.argv)
+app.setApplicationName("EGG player")
+
+plik="/home/marek/Wideo/d.avi"
+zrodlo=Phonon.MediaSource(plik)
+mediaObject = Phonon.MediaObject()
+videoWidget = Phonon.VideoWidget()
+Phonon.createPath(mediaObject, videoWidget)
+mediaObject.setQueue(zrodlo)
+
+mediaObject.play()
+mediaObject.setVisible(True)
 
 
-class MainWindow(QtGui.QMainWindow):
-	def __init__(self):
-		super(QtGui.QMainWindow, self).__init__()
-	
-		plik="/home/marek/Wideo/d.avi"
-		player=Phonon.VideoPlayer(Phonon.VideoCategory)
-		zrodlo=Phonon.MediaSource(plik)
-		self.mediaObject = Phonon.MediaObject(self)
-		self.videoWidget = Phonon.VideoWidget(self)
-		Phonon.createPath(self.mediaObject, self.videoWidget)
-
+while True:
+	s=input("ttttt")
+	if s==1:
+		player.pause()
+	elif s==2:
 		player.play(zrodlo)
-		player.setVisible(True)
+	elif s==3:
+		player.stop()
+	elif s==4:
+		if not self.videoWidget.isFullScreen():
+			self.videoWidget.enterFullScreen()
+			self.videoWidget.showFullScreen()
+	else:
+		self.videoWidget.exitFullScreen()
 		
+#if __name__ == '__main__':
+	
 
-		while True:
-			s=input("ttttt")
-			if s==1:
-				player.pause()
-			elif s==2:
-				player.play(zrodlo)
-			elif s==3:
-				player.stop()
-			elif s==4:
-				if not self.videoWidget.isFullScreen():
-					self.videoWidget.enterFullScreen()
-					self.videoWidget.showFullScreen()
-			else:
-				self.videoWidget.exitFullScreen()
-				
-if __name__ == '__main__':
-	while True:
-		app = QApplication(sys.argv)
-		app.setApplicationName("EGG player")
-		egg=MainWindow()
-		egg.show()
-		if s==4:
-			egg.show()
+
 	sys.exit(app.exec_())
